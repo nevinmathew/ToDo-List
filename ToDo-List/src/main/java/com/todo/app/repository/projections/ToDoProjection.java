beca774065c04619e756ec53c29481c859d0c1be
nevinmathew <.>
2023-09-19 11:00:00 +0530

    OpenAPI and create task method updated

---
+++ ToDo-List/src/main/java/com/todo/app/repository/projections/ToDoProjection.java
@@ -0,0 +1,27 @@
+package com.todo.app.repository.projections;
+
+import java.time.LocalDateTime;
+
+public interface ToDoProjection {
+
+	public int getTaskId();
+
+	public String getTaskName();
+	
+	public String getDescription();
+
+	public LocalDateTime getCreatedTimestamp();
+
+	public LocalDateTime getUpdatedTimestamp();
+
+	public LocalDateTime getTargetTimestamp();
+
+	public String getPriority();
+	
+	public String getStatus();
+
+	public int getCategoryId();
+
+	public String getCategoryName();
+	
+}
--- ToDo-List/src/main/java/com/todo/app/service/impl/ToDoService.java
+++ ToDo-List/src/main/java/com/todo/app/service/impl/ToDoService.java
@@ -5,13 +5,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.todo.app.dto.ToDoDto;
+import com.todo.app.entity.Category;
 import com.todo.app.entity.Tasks;
 import com.todo.app.entity.enums.Priority;
 import com.todo.app.entity.enums.Status;
+import com.todo.app.repository.CategoryRepository;
 import com.todo.app.repository.TaskRepository;
 import com.todo.app.repository.projections.ToDoProjection;
 import com.todo.app.service.IToDoService;
@@ -21,6 +26,12 @@ public class ToDoService implements IToDoService{
 
 	@Autowired
 	TaskRepository taskRepo;
+	
+	@Autowired
+	CategoryRepository categoryRepo;
+	
+//	@Autowired
+//	EntityManager entityManager;
 	
 	@Override
 	public List<ToDoProjection> showToDos() {
@@ -37,6 +48,7 @@ public class ToDoService implements IToDoService{
 	}
 
 	@Override
+	@Transactional
 	public String createToDo(ToDoDto toDo) {
 		Tasks task = new Tasks();
 		task.setTaskName(toDo.getTaskName());
@@ -46,20 +58,26 @@ public class ToDoService implements IToDoService{
 			switch(toDo.getPriority().toLowerCase()) {
 				case "low":
 					task.setPriority(Priority.low.name());
+					break;
 				case "medium":
 					task.setPriority(Priority.medium.name());
+					break;
 				case "high":
 					task.setPriority(Priority.high.name());
+					break;
 			}
 		
 		if(toDo.getStatus()!=null && !toDo.getStatus().isEmpty())
 			switch(toDo.getStatus().toLowerCase()){
 				case "pending":
 					task.setStatus(Status.pending.name());
+					break;
 				case "inprogress":
 					task.setStatus(Status.inProgress.name());
+					break;
 				case "completed":
 					task.setStatus(Status.completed.name());
+					break;
 			}
 		
 		task.setCreatedTimestamp(LocalDateTime.now());
@@ -67,29 +85,33 @@ public class ToDoService implements IToDoService{
 		if(task.getTargetTimestamp()!=null)
 			task.setTargetTimestamp(toDo.getTargetTimestamp());
 		
-//		task.setCategory(toDo.getCategoryId());
+//		task.setCategory(entityManager.getReference(Category.class, toDo.getCategoryId()));
+		task.setCategory(categoryRepo.findById(toDo.getCategoryId()).get());
 		
 		taskRepo.save(task);
 		return "The toDo has been created.";
 	}
 
 	@Override
-	public ToDoDto updateToDo(int id, ToDoDto toDo) {
-		Optional<Tasks> task = taskRepo.findById(id);
-		task.get().setTaskName(toDo.getTaskName());
-		task.get().setDescription(toDo.getDescription());
+	public ToDoDto updateToDo(int id, ToDoDto toDo) { 
+		Optional<Tasks> task = taskRepo.findById(id); 
+		task.get().setTaskName(toDo.getTaskName()); 
+		task.get().setDescription(toDo.getDescription()); 
 		
-		if(toDo.getPriority()!=null && !toDo.getPriority().isEmpty())
-			switch(toDo.getPriority().toLowerCase()) {
-				case "low":
-					task.get().setPriority(Priority.low.name());
-					toDo.setPriority(Priority.low.name());
+		if(toDo.getPriority()!=null && !toDo.getPriority().isEmpty()) 
+			switch(toDo.getPriority().toLowerCase()) { 
+				case "low": 
+					task.get().setPriority(Priority.low.name()); 
+					toDo.setPriority(Priority.low.name()); 
+					break;
 				case "medium":
 					task.get().setPriority(Priority.medium.name());
 					toDo.setPriority(Priority.medium.name());
+					break;
 				case "high":
 					task.get().setPriority(Priority.high.name());
 					toDo.setPriority(Priority.high.name());
+					break;
 			}
 		
 		if(toDo.getStatus()!=null && !toDo.getStatus().isEmpty())
@@ -97,20 +119,23 @@ public class ToDoService implements IToDoService{
 				case "pending":
 					task.get().setStatus(Status.pending.name());
 					toDo.setStatus(Status.pending.name());
+					break;
 				case "inprogress":
 					task.get().setStatus(Status.inProgress.name());
 					toDo.setStatus(Status.inProgress.name());
+					break;
 				case "completed":
 					task.get().setStatus(Status.completed.name());
 					toDo.setStatus(Status.completed.name());
-			}
-	
-		if(task.get().getTargetTimestamp()!=null)
-			task.get().setTargetTimestamp(toDo.getTargetTimestamp());
+					break;
+			} 
+
+		if(task.get().getTargetTimestamp()!=null) 
+			task.get().setTargetTimestamp(toDo.getTargetTimestamp()); 
 		
-		if(task.isPresent()) {
-			task.get().setUpdatedTimestamp(LocalDateTime.now());
-			toDo.setUpdatedTimestamp(task.get().getUpdatedTimestamp());
+		if(task.isPresent()) { 
+			task.get().setUpdatedTimestamp(LocalDateTime.now()); 
+			toDo.setUpdatedTimestamp(task.get().getUpdatedTimestamp()); 
 		}
 		
 		taskRepo.save(task.get());
