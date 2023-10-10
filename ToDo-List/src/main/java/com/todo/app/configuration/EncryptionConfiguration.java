938d40ff3a54c6755d233c77dbbbbc6507664bc7
nevin <.>
2023-10-10 18:55:56 +0530

    updated docs in pom and service, configuration in yml, banner and encryption

--- ToDo-List/src/main/java/com/todo/app/configuration/EncryptionConfiguration.java
+++ ToDo-List/src/main/java/com/todo/app/configuration/EncryptionConfiguration.java
@@ -1,10 +1,13 @@
 package com.todo.app.configuration;
 
-import org.jasypt.encryption.StringEncryptor;
+import javax.sql.DataSource;
+
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * Configuration class for encryption settings.
@@ -20,12 +23,31 @@ public class EncryptionConfiguration  {
      *
      * @return A configured StringEncryptor.
      */
+//	@Bean
+//	public StringEncryptor stringEncryptor() {
+//	  StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//	  encryptor.setPassword(myKey);
+//	  return encryptor;
+//	}
+	
 	@Bean
-	public StringEncryptor stringEncryptor() {
-	  StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-	  encryptor.setPassword("${MY_KEY_VAR}");
-	  return encryptor;
-	}
+    public DataSource dataSource(Environment environment) {
+        String encryptedPassword = environment.getProperty("spring.datasource.password");
+        String password = encryptor().decrypt(encryptedPassword);
+
+        return DataSourceBuilder.create()
+                .url(environment.getProperty("spring.datasource.url"))
+                .username(environment.getProperty("spring.datasource.username"))
+                .password(password)
+                .build();
+    }
+
+    @Bean
+    public StandardPBEStringEncryptor encryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(myKey);
+        return encryptor;
+    }
 
 	/**
      * Print the decrypted password.
@@ -34,7 +56,7 @@ public class EncryptionConfiguration  {
         try{
         	String encryptedPassword = "I0BXUCm0vCiASShRUucwIA==";
         	System.out.println("decryption key: " + myKey);
-	        String decryptedPassword = stringEncryptor().decrypt(encryptedPassword);
+	        String decryptedPassword = encryptor().decrypt(encryptedPassword);
 	        System.out.println("Decrypted Password: " + decryptedPassword);
         } catch (Exception e) {
         	e.printStackTrace();
