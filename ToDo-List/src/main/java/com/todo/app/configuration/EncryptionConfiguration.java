68c331abd55b774f4d924282c0430cd072b917f7
nevinmathew <88356497+nevinmathew@users.noreply.github.com>
2023-12-11 18:02:27 +0530

    encryption disabled

--- ToDo-List/src/main/java/com/todo/app/configuration/EncryptionConfiguration.java
+++ ToDo-List/src/main/java/com/todo/app/configuration/EncryptionConfiguration.java
@@ -1,71 +1,71 @@
-package com.todo.app.configuration;
-
-import javax.sql.DataSource;
-
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-
-/**
- * Configuration class for encryption settings.
- */
-@Configuration
-public class EncryptionConfiguration  {
-
-	@Value("${jasypt.encryptor.password}")
-	String myKey;
-
-	/**
-     * Configures and provides a StringEncryptor bean.
-     *
-     * @return A configured StringEncryptor.
-     */
+//package com.todo.app.configuration;
+//
+//import javax.sql.DataSource;
+//
+//import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//
+///**
+// * Configuration class for encryption settings.
+// */
+//@Configuration
+//public class EncryptionConfiguration  {
+//
+//	@Value("${jasypt.encryptor.password}")
+//	String myKey;
+//
+//	/**
+//     * Configures and provides a StringEncryptor bean.
+//     *
+//     * @return A configured StringEncryptor.
+//     */
+////	@Bean
+////	public StringEncryptor stringEncryptor() {
+////	  StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+////	  encryptor.setPassword(myKey);
+////	  return encryptor;
+////	}
+//	
+//	/**
+//     * Configures and provides a DataSource bean.
+//     *
+//     * @return A configured DataSource.
+//     */
 //	@Bean
-//	public StringEncryptor stringEncryptor() {
-//	  StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-//	  encryptor.setPassword(myKey);
-//	  return encryptor;
-//	}
-	
-	/**
-     * Configures and provides a DataSource bean.
-     *
-     * @return A configured DataSource.
-     */
-	@Bean
-    public DataSource dataSource(Environment environment) {
-        String encryptedPassword = environment.getProperty("spring.datasource.password");
-        String password = encryptor().decrypt(encryptedPassword);
-
-        return DataSourceBuilder.create()
-                .url(environment.getProperty("spring.datasource.url"))
-                .username(environment.getProperty("spring.datasource.username"))
-                .password(password)
-                .build();
-    }
-
-    @Bean
-    public StandardPBEStringEncryptor encryptor() {
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword(myKey);
-        return encryptor;
-    }
-
-	/**
-     * Print the decrypted password.
-     */
-    public void printDecryptedPassword() {
-        try{
-        	String encryptedPassword = "I0BXUCm0vCiASShRUucwIA==";
-        	System.out.println("decryption key: " + myKey);
-	        String decryptedPassword = encryptor().decrypt(encryptedPassword);
-	        System.out.println("Decrypted Password: " + decryptedPassword);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        }
-    }
-}
-
+//    public DataSource dataSource(Environment environment) {
+//        String encryptedPassword = environment.getProperty("spring.datasource.password");
+//        String password = encryptor().decrypt(encryptedPassword);
+//
+//        return DataSourceBuilder.create()
+//                .url(environment.getProperty("spring.datasource.url"))
+//                .username(environment.getProperty("spring.datasource.username"))
+//                .password(password)
+//                .build();
+//    }
+//
+//    @Bean
+//    public StandardPBEStringEncryptor encryptor() {
+//        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        encryptor.setPassword(myKey);
+//        return encryptor;
+//    }
+//
+//	/**
+//     * Print the decrypted password.
+//     */
+//    public void printDecryptedPassword() {
+//        try{
+//        	String encryptedPassword = "I0BXUCm0vCiASShRUucwIA==";
+//        	System.out.println("decryption key: " + myKey);
+//	        String decryptedPassword = encryptor().decrypt(encryptedPassword);
+//	        System.out.println("Decrypted Password: " + decryptedPassword);
+//        } catch (Exception e) {
+//        	e.printStackTrace();
+//        }
+//    }
+//}
+//
--- ToDo-List/src/main/java/com/todo/app/configuration/Runner.java
+++ ToDo-List/src/main/java/com/todo/app/configuration/Runner.java
@@ -1,27 +1,27 @@
-package com.todo.app.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-/**
- * The `Runner` class is a `CommandLineRunner` component.
- * It is responsible for executing tasks at startup.
- */
-@Component
-public class Runner implements CommandLineRunner {
-
-    @Autowired
-    private EncryptionConfiguration myConfig;
-
-    /**
-     * This method is invoked when the application starts.
-     * It runs the `printDecryptedPassword` method from the `EncryptionConfiguration` bean.
-     *
-     * @param args Command-line arguments (not used in this case).
-     */
-    @Override
-    public void run(String... args) {
-        myConfig.printDecryptedPassword();
-    }
-}
+//package com.todo.app.configuration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * The `Runner` class is a `CommandLineRunner` component.
+// * It is responsible for executing tasks at startup.
+// */
+//@Component
+//public class Runner implements CommandLineRunner {
+//
+//    @Autowired
+//    private EncryptionConfiguration myConfig;
+//
+//    /**
+//     * This method is invoked when the application starts.
+//     * It runs the `printDecryptedPassword` method from the `EncryptionConfiguration` bean.
+//     *
+//     * @param args Command-line arguments (not used in this case).
+//     */
+//    @Override
+//    public void run(String... args) {
+//        myConfig.printDecryptedPassword();
+//    }
+//}
